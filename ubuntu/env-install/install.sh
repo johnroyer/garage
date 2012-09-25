89ff8d803c9b787d9832fba69d7a19d40eeaf280
Zero <john.royer@gmail.com>
2012-09-25 15:50:32 +0800

    Script for Environment Preparation
    Install environment when default Ubuntu 10.04 installation finished.

---
+++ ubuntu/env-install/install.sh
@@ -0,0 +1,13 @@
+# Script for environment install upon default Ubuntu 10.04
+# installed.
+
+# Develope tools
+aptitue install build-essential
+
+# Firestarter
+aptitude install firestarter
+
+# Install GCC 4.7
+add-apt-repository ppa:ubuntu-toolchain-r/test
+aptitude install gcc-4.7 g++-4.7
+
