5978ce05f3842c4369d5658fa8e48ca23469e83a
Zero <john.royer@gmail.com>
2013-07-22 22:38:36 +0800

    Adding Tools

--- ubuntu/env-install/install.sh
+++ ubuntu/env-install/install.sh
@@ -4,10 +4,12 @@
 # Develope tools
 aptitue install build-essential
 
-# Firestarter
-aptitude install firestarter
-
 # Install GCC 4.7
 add-apt-repository ppa:ubuntu-toolchain-r/test
 aptitude install gcc-4.7 g++-4.7
 
+# Firestarter
+aptitude install firestarter
+
+# System tools
+aptitude install tree
